e124aa4efa2d17496bd3cda33bd78742dfa9f22b
buyath <buyaththayub@gmail.com>
2026-02-01 11:47:40 +0000

    initial commit

---
+++ java-basics/hello-world.java
@@ -0,0 +1,6 @@
+public class Thayub{
+    public static void main(String[] args){
+        String  name = "Thayub";
+        System.out.println("Hello" + " " + name);
+    }
+}
---
+++ java-basics/test.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        // System.out.print("H")
+        int num = 12;
+        System.out.println(num);
+    }
+}

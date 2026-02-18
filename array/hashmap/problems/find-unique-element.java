67fce0246d860fe98b792b4d67108ec0ba657cb9
buyath <buyaththayub@gmail.com>
2026-02-18 04:26:24 +0000

    feat: i've solved find unique element problem using hashmap

--- array/hashmap/problems/find-unique-element.java
+++ array/hashmap/problems/find-unique-element.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
     public static void main(String[] args){
@@ -8,11 +9,11 @@ public class Main{
             unique.put(arr[i], unique.getOrDefault(arr[i],0) + 1);
         }
         
-        for(int number : unique.values()){
-            if(number == 1){
-                System.out.println(number);
-            }
+       for(Map.Entry<Integer,Integer> number : unique.entrySet()){
+        if(number.getValue() == 1){
+            System.out.println(number.getKey());
         }
+       }
     }
 }
 

a25d9da2f0961568121d05dd87e16a4de31c4c81
buyath <buyaththayub@gmail.com>
2026-02-19 07:49:27 +0000

    dsa: implement two sum with hashmap (O(n))

---
+++ array/hashmap/problems/two-sum.js
@@ -0,0 +1,19 @@
+// function twoSum(arr,target){
+//     let map = new Map();
+//     for(let i = 0; i<arr.length; i++){
+//         let complement  = target - arr[i];
+//         if(map.has(complement)){
+//             return [map.get(complement)];
+//         }
+//         map.set(arr[i],i);
+//     }
+
+// }
+// console.log(twoSum([2,7,11,15],9));
+
+let  map = new Map();
+
+map.set("name",1);
+map.set("mohideen",2);
+
+console.log(map.has("name"));

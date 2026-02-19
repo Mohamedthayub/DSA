0154ea845d8daa7d7c172eae2bc78134a125f684
buyath <buyaththayub@gmail.com>
2026-02-19 15:38:59 +0000

    solved unique number occurence problem using hashmap

---
+++ array/hashmap/problems/unique-number-occurence.js
@@ -0,0 +1,46 @@
+function findUnique(arr){
+    let map = new Map();
+    for(let i = 0; i<arr.length; i++){
+        if(!map.has(arr[i])){
+            map.set(arr[i],1);
+        }
+        else{
+            map.set(arr[i],map.get(arr[i]) + 1);
+        }
+    }
+    let freq  = [];
+    for(let [key,value] of map){
+        if(!freq.includes(value)){
+            freq.push(value);
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(findUnique([1,2]));
+
+
+
+/*
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+*/
--- array/hashmap/test.java
+++ array/hashmap/test.java
@@ -4,8 +4,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<Integer, Integer> numbers = new HashMap<>();
-        numbers.put(1, 10);
         numbers.put(2, 10);
         numbers.put(3, 10);
         
@@ -14,10 +12,12 @@ public class Main {
         people.put("thayub", 20);
         people.put("mohideen",19);
         people.put("halik",25);
-
         
-        for(Map.Entry<String,Integer> person : people.entrySet()){
-            System.out.println(person.getKey() + " -> " + person.getValue());
-        } 
+        for(Map.Entry <String,Integer>  number  : people.
+        HashMap<Integer, Integer> numbers = new HashMap<>();
+        numbers.put(1, 10);entrySet()){
+            System.out.println(number.getKey() + " " + number.getValue());
+        }
+        
     }
 }

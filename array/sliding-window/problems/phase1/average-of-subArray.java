323ba38b6010fe80baa53fc179f20b2cc5dd812b
buyath <buyaththayub@gmail.com>
2026-02-25 02:51:07 +0000

    solved average subarray k size

---
+++ array/sliding-window/problems/phase1/average-of-subArray.java
@@ -0,0 +1,8 @@
+public class Main{
+    static int subArrayAverage(int[] ,int k ){
+        
+    }
+    public static void main(String[] args){
+        
+    }
+}
--- array/sliding-window/problems/test.js
+++ array/sliding-window/problems/test.js
@@ -1,43 +1,37 @@
-function   subArray(arr,k){
-    if(arr.length == 0 || k > arr.length  || k <= 0){
+function averageSubArray(arr,k){
+    let average = [];
+    if(arr.length == 0 || k <= 0 || k > arr.length ){
         return null;
     }
-    let maxSum = -Infinity;
-    for(let i  = 0 ; i<=arr.length - k;  i++){
-        let sum = 0;
-        for(let j = i; j < k + i; j++){
-            sum = sum + arr[j];
-        }
-        if(sum > maxSum){
-            maxSum = sum;
-        }
+    let windowSum = 0;
+    for(let i = 0; i<k; i++){
+        windowSum = windowSum + arr[i];
     }
-    return maxSum;
+    for(let i = k; i<arr.length; i++){
+        average.push(windowSum / k);
+        windowSum = windowSum + arr[i];
+        windowSum = windowSum - arr[i-k];
+    }
+    average.push(windowSum / k);
+    return average;
 }
-console.log(subArray([4,4,4],0));
-console.log(subArray([2,3],1));
-console.log(subArray([100,200,300,400],2));
-
-
+console.log(averageSubArray([1,3,2,6,-1,4,1,8,2],5));
 
 
 /*
-Given an array of integers arr[] and an integer k, find the maximum possible sum among all contiguous subarrays of size exactly k.
-A subarray is a sequence of consecutive elements from the original array. Return the maximum sum that can be obtained from any such subarray of length k.
+2️⃣ Average of Subarrays of Size k
+📌 Problem Statement
 
-Examples: 
+Given an array of integers and an integer k, calculate the average of all contiguous subarrays of size k.
 
-Input  : arr[] = [100, 200, 300, 400],  k = 2
-Output : 700
-Explanation: We get maximum sum by adding subarray [300,400] of size 2
+🔹 Example
+Input:
+arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
+k = 5
 
-Input  : arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4 
-Output : 39
-Explanation: We get maximum sum by adding subarray [4, 2, 10, 23] of size 4.
+Output:
+[2.2, 2.8, 2.4, 3.6, 2.8]
+🔹 Explanation
 
-Input  : arr[] = [2, 3], k = 1
-Output : 3
-Explanation: The subarrays of size 1 are [2] and [3]. The maximum sum is 3.
-
-
+Each output value represents the average of one window of size 5.
 */

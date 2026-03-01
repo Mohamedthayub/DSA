8a9d6144028e9f57896a6b231d29d6895f4f6772
buyath <buyaththayub@gmail.com>
2026-03-01 01:16:02 +0000

    solved maxConsecutiveOnes problem in simple travesal pattern

--- array/sliding-window/problems/test.js
+++ array/sliding-window/problems/test.js
@@ -1,23 +1,22 @@
 function averageSubArray(arr,k){
-    let average = [];
-    if(arr.length == 0 || k <= 0 || k > arr.length ){
-        return null;
+    if(arr.length == 0  || k <= 0 || k > arr.length){
+        return ;
     }
     let windowSum = 0;
     for(let i = 0; i<k; i++){
         windowSum = windowSum + arr[i];
     }
-    for(let i = k; i<arr.length; i++){
+    let average = [];
+
+    for(let j = k; j<arr.length; j++){
         average.push(windowSum / k);
-        windowSum = windowSum + arr[i];
-        windowSum = windowSum - arr[i-k];
+        windowSum  = windowSum +  arr[j];
+        windowSum = windowSum  - arr[j-k]; 
     }
     average.push(windowSum / k);
     return average;
 }
-console.log(averageSubArray([1,3,2,6,-1,4,1,8,2],5));
-
-
+console.log(averageSubArray( [1, 3, 2, 6, -1, 4, 1, 8, 2],5));
 /*
 2️⃣ Average of Subarrays of Size k
 📌 Problem Statement
@@ -28,6 +27,8 @@ Given an array of integers and an integer k, calculate the average of all contig
 Input:
 arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 k = 5
+
+
 
 Output:
 [2.2, 2.8, 2.4, 3.6, 2.8]
---
+++ array/sliding-window/problems/test2.js
@@ -0,0 +1,40 @@
+function maxSum(arr, k){
+    if(arr.length == 0 || k <= 0 ||  k > arr.length){
+        return null;
+    }
+    let maxSum = 0;
+    for(let i  = 0; i< k; i++){
+        maxSum = maxSum + arr[i];
+    }
+    let windowSum =  maxSum;
+    for(let j = k; j<arr.length; j++){
+        windowSum = windowSum + arr[j];
+        windowSum = windowSum - arr[j-k];
+        
+        maxSum = Math.max(maxSum,windowSum);
+    }
+    return maxSum;
+
+}
+console.log(maxSum([100,200,300,400],2));
+
+/*
+Given an array of integers arr[] and an integer k, find the maximum possible sum among all contiguous subarrays of size exactly k.
+A subarray is a sequence of consecutive elements from the original array. Return the maximum sum that can be obtained from any such subarray of length k.
+
+Examples: 
+
+Input  : arr[] = [100, 200, 300, 400],  k = 2
+Output : 700
+Explanation: We get maximum sum by adding subarray [300,400] of size 2
+
+Input  : arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4 
+Output : 39
+Explanation: We get maximum sum by adding subarray [4, 2, 10, 23] of size 4.
+
+Input  : arr[] = [2, 3], k = 1
+Output : 3
+Explanation: The subarrays of size 1 are [2] and [3]. The maximum sum is 3.
+
+
+*/

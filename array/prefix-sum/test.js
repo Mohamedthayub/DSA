2050e90c013fdcd9143910e2c4ae58c5e4507fd7
buyath <buyaththayub@gmail.com>
2026-03-11 02:09:31 +0000

    solved pivot index problem using prefix sum algo

--- array/prefix-sum/test.js
+++ array/prefix-sum/test.js
@@ -1,17 +1,8 @@
-// let arr = [1,2,3,4,5];
-// let prefix = [];
-// prefix[0] = arr[0];
-// for(let i = 1; i < arr.length ; i++){
-//     prefix[i] = prefix[i-1] + arr[i];
-// }
-// console.log(prefix);
-
-// RunninSum Problem
-
-let nums = [1,2,3,4,5];
+let arr = [1,2,3,4,5];
+let l = 0;
+let r = 3;
 let sum = 0;
-for(let i = 0; i<nums.length; i++){
-    sum = sum + nums[i];
-    nums[i] = sum;
+for(let i = l; i<=r; i++){
+    sum = sum + arr[i];
 }
-console.log(nums);
+console.log(sum);

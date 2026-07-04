1e0ade6ef0fac7c020842ee0a193047e6fc85cf2
buyath <buyaththayub@gmail.com>
2026-07-04 12:15:50 +0000

    revised 10 basic problems

---
+++ revition/basic-problems/exercise8.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int numbers = 123456;
+        int count = 0;
+        while(numbers > 0){
+            count = count + 1;
+            numbers = numbers / 10;
+        }
+        System.out.println(count);
+    }
+}
+// Count the digits in a number.

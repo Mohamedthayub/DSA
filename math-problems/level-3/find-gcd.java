718e19f6f1ee12de2a961f5c5c23c962e283f206
buyath <buyaththayub@gmail.com>
2026-09-01 14:15:38 +0000

    solved lcm problem in math

---
+++ math-problems/level-3/find-gcd.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 24;
+        int b = 36;
+        int gcd = 1;
+        for(int i = 1; i<=b; i++){
+            if(a % i == 0 && b % i == 0){
+                gcd = i;
+            }
+        }
+        System.out.println("GCD :" +  " " + gcd);
+    }
+}

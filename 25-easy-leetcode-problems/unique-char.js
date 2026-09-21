5111ede04f0d3c4af03ceb16d7e97ee531ac4d08
buyath <buyaththayub@gmail.com>
2026-09-21 23:16:12 +0000

    solved unique-char problem

--- 25-easy-leetcode-problems/unique-char.js
+++ 25-easy-leetcode-problems/unique-char.js
@@ -7,13 +7,42 @@ function unique_character(str){
         else{
             unique.set(str[i],unique.get(str[i]) + 1);
         }
-    }
+    } 
     for(let [key,value] of unique){
         if(value == 1){
-            return key;
+            return str.indexOf(key);
         }
     }
     return -1;
 }
 console.log(unique_character("leetcode"))
-console.log(unique_character("aabb"));
+// console.log(unique_character("aabb"))
+
+
+/*
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Example 1:
+
+Input: s = "leetcode"
+
+Output: 0
+
+Explanation:
+
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+
+Input: s = "loveleetcode"
+
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+
+Output: -1
+
+ 
+*/
